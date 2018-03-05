2f947db561538477631c49d41747d73259e7ed5b
Yann Cuttaz <y.cuttaz@gmail.com>
2018-03-05 10:03:56 +0100

    cordova plugin updated with all methods

--- src/android/EduIDPlugin.java
+++ src/android/EduIDPlugin.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 
+import com.eduid.Library.DataAccess.EduIDLibraryException;
 import com.eduid.Library.DataAccess.SwissEduIDCommunication;
 import com.eduid.Library.IdNativeAppIntegrationLayer;
 
@@ -35,6 +36,15 @@ public class EduIDPlugin extends CordovaPlugin {
 
     private Context context = null;
     public static final String ACTION_AUTHORIZE_PROTOCOLS = "authorizeProtocols";
+    public static final String ACTION_SERVICES_NAMES = "serviceNames";
+    public static final String ACTION_GET_ENDPOINT_URL = "getEndpointURL";
+    public static final String ACTION_GET_DISPLAY_NAME = "getDisplayName";
+    public static final String ACTION_GET_SERVICE_TOKEN = "getServiceToken";
+    public static final String ACTION_GET_SERVICE_URL = "getServiceUrl";
+    public static final String ACTION_REMOVE_SERVICE = "removeService";
+    public static final String ACTION_CLEAR_ALL_SERVICES = "clearAllServices";
+    public static final String ACTION_SERIALIZE = "serialize";
+    public static final String ACTION_PARSE = "parse";
     private Activity currentActivity = null;
 
     // NAIL instance
@@ -48,6 +58,9 @@ public class EduIDPlugin extends CordovaPlugin {
             this.context = this.cordova.getActivity().getApplicationContext();
 
         try {
+            /**
+             * authorizeProtocols
+             */
             if (ACTION_AUTHORIZE_PROTOCOLS.equals(action)) {
 
                 /**
@@ -69,7 +82,7 @@ public class EduIDPlugin extends CordovaPlugin {
                         Intent openCurrentActivity = new Intent(EduIDPlugin.this.currentActivity, EduIDPlugin.this.currentActivity.getClass());
                         openCurrentActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
                         EduIDPlugin.this.context.startActivity(openCurrentActivity);
-                        callbackContext.success("TOKEN");
+                        callbackContext.success();
                     }
 
                     @Override
@@ -80,7 +93,163 @@ public class EduIDPlugin extends CordovaPlugin {
 
                 return true;
 
-            }else { // Action not found
+            }
+
+            /**
+             * serviceNames
+             */
+            else if(ACTION_SERVICES_NAMES.equals(action)){
+                callbackContext.success(getNAIL().serviceNames());
+            }
+
+
+            /**
+             * getEndpointURL
+             */
+            else if(ACTION_GET_ENDPOINT_URL.equals(action)){
+
+
+                /**
+                 * Get args
+                 */
+                JSONObject args_obj = args.getJSONObject(0);
+
+                /**
+                 * Get request attributes
+                 */
+                String serviceName = args_obj.getString("serviceName");
+                String protocolName = args_obj.getString("protocolName");
+
+
+                callbackContext.success(getNAIL().getEndpointURL(serviceName, protocolName));
+
+
+            }
+
+
+            /**
+             * getDisplayName
+             */
+            else if(ACTION_GET_DISPLAY_NAME.equals(action)){
+                /**
+                 * Get args
+                 */
+                JSONObject args_obj = args.getJSONObject(0);
+
+                /**
+                 * Get request attributes
+                 */
+                String serviceName = args_obj.getString("serviceName");
+
+
+                callbackContext.success(getNAIL().getDisplayName(serviceName));
+            }
+
+
+            /**
+             * getServiceToken
+             */
+            else if(ACTION_GET_SERVICE_TOKEN.equals(action)){
+                /**
+                 * Get args
+                 */
+                JSONObject args_obj = args.getJSONObject(0);
+
+                /**
+                 * Get request attributes
+                 */
+                String serviceName = args_obj.getString("serviceName");
+                String protocolName = args_obj.getString("protocolName");
+
+                callbackContext.success(getNAIL().getServiceToken(serviceName, protocolName));
+            }
+
+
+            /**
+             * getServiceURL
+             */
+            else if(ACTION_GET_SERVICE_URL.equals(action)){
+                /**
+                 * Get args
+                 */
+                JSONObject args_obj = args.getJSONObject(0);
+
+                /**
+                 * Get request attributes
+                 */
+                String serviceName = args_obj.getString("serviceName");
+                try {
+                    callbackContext.success(getNAIL().getServiceUrl(serviceName));
+                } catch(EduIDLibraryException exception){
+                    callbackContext.error(exception.getMessage());
+                }
+            }
+
+
+            /**
+             * removeService
+             */
+            else if(ACTION_REMOVE_SERVICE.equals(action)){
+                /**
+                 * Get args
+                 */
+                JSONObject args_obj = args.getJSONObject(0);
+
+                /**
+                 * Get request attributes
+                 */
+                String serviceName = args_obj.getString("serviceName");
+
+                callbackContext.success(getNAIL().removeService(serviceName));
+            }
+
+
+            /**
+             * clearAllServices
+             */
+            else if(ACTION_CLEAR_ALL_SERVICES.equals(action)){
+                getNAIL().clearAllServices();
+                callbackContext.success();
+            }
+
+
+            /**
+             * serialize
+             */
+            else if(ACTION_SERIALIZE.equals(action)){
+                callbackContext.success(getNAIL().serialize());
+            }
+
+
+            /**
+             * parse
+             */
+            else if(ACTION_PARSE.equals(action)){
+                /**
+                 * Get args
+                 */
+                JSONObject args_obj = args.getJSONObject(0);
+
+                /**
+                 * Get request attributes
+                 */
+                String serviceSpec = args_obj.getString("serviceSpec");
+
+                try {
+                    getNAIL().parse(serviceSpec);
+                    callbackContext.success();
+                } catch(JSONException jsonException){
+                    callbackContext.error(jsonException.getMessage());
+                }
+                catch(EduIDLibraryException exception){
+                    callbackContext.error(exception.getMessage());
+                }
+            }
+
+
+
+
+            else { // Action not found
                 callbackContext.error("Invalid action");
                 return false;
             }
--- www/EduIDPlugin.js
+++ www/EduIDPlugin.js
@@ -9,7 +9,117 @@ var EduIDPlugin = {
                 "protocols": protocols
             }]
         );
+     },
+
+     serviceNames: function(successCallback){
+        cordova.exec(
+            successCallback, // success callback function
+            null, // error callback function
+            'EduIDPlugin', // Java Class
+            'serviceNames', // action name
+            []
+        );
+     },
+
+    getEndpointURL: function(serviceName, protocolName, successCallback){
+        cordova.exec(
+            successCallback, // success callback function
+            null, // error callback function
+            'EduIDPlugin', // Java Class
+            'getEndpointURL', // action name
+            [{                  // args
+                "serviceName": serviceName,
+                "protocolName": protocolName
+            }]
+        );
+     },
+
+    getDisplayName: function(serviceName, successCallback){
+        cordova.exec(
+            successCallback, // success callback function
+            null, // error callback function
+            'EduIDPlugin', // Java Class
+            'getDisplayName', // action name
+            [{                  // args
+                "serviceName": serviceName
+            }]
+        );
+     },
+
+     getServiceToken: function(serviceName, protocolName, successCallback){
+        cordova.exec(
+            successCallback, // success callback function
+            null, // error callback function
+            'EduIDPlugin', // Java Class
+            'getServiceToken', // action name
+            [{                  // args
+                "serviceName": serviceName,
+                "protocolName": protocolName
+            }]
+        );
+     },
+
+     getServiceUrl: function(serviceName, successCallback, errorCallback){
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'EduIDPlugin', // Java Class
+            'getServiceUrl', // action name
+            [{                  // args
+                "serviceName": serviceName
+            }]
+        );
+     },
+
+     removeService: function(serviceName, successCallback){
+        cordova.exec(
+            successCallback, // success callback function
+            null, // error callback function
+            'EduIDPlugin', // Java Class
+            'removeService', // action name
+            [{                  // args
+                "serviceName": serviceName
+            }]
+        );
+     },
+
+     clearAllServices: function(successCallback){
+        cordova.exec(
+            successCallback, // success callback function
+            null, // error callback function
+            'EduIDPlugin', // Java Class
+            'clearAllServices', // action name
+            []
+        );
+     },
+
+     serialize: function(successCallback){
+        cordova.exec(
+            successCallback, // success callback function
+            null, // error callback function
+            'EduIDPlugin', // Java Class
+            'serialize', // action name
+            []
+        );
+     },
+
+     parse: function(serviceSpec, successCallback, errorCallback){
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'EduIDPlugin', // Java Class
+            'parse', // action name
+            [{                  // args
+                "serviceSpec": serviceSpec
+            }]
+        );
      }
+
+
+
+
+
+
 }
 
 module.exports = EduIDPlugin;
